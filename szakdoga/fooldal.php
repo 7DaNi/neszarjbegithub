9b1c570d14ccd64f6496ddd6854ea0bbadf6c9b8
7DaNi <daniili9393@gmail.com>
2014-03-23 00:50:36 +0100

    123

---
+++ szakdoga/fooldal.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="hu" />
+<meta name="description" content="szakdolgozat">
+<title>Szakdolgozat</title>
+<script type="text/javascript" src="swfobject.js"></script>
+<script type="text/javascript" src="banner.js"></script>
+</head>
+<body>
+<div id="swf_content">
+	<p>Frissítés alatt.</p>
+</div>
+<?php
+session_start();
+require_once('head.php');
+if (isset($_GET['nev'])){
+	$nev = $_GET['nev'];
+	$_SESSION["nev"]=$nev;
+	}
+
+	if(isset($_POST["belepes"])){
+		$user = $_POST["user"];
+		$pw = $_POST["pw"];
+		
+		$dbc = mysqli_connect(host,user,pw,db);
+		mysqli_query($dbc,"SET NAME utf8");
+		$query = "SELECT * FROM userek WHERE user='$user' AND pw='$pw'";
+		$data = mysqli_query($dbc,$query);
+		
+		if(mysqli_num_rows($data)==1){
+			$adatok = mysqli_fetch_array($data);
+			$user = $adatok["user"];
+			$pw = $adatok["pw"];
+			$rang = $adatok["rang"];
+			$nev = $adatok["nev"];
+			
+			
+			$_SESSION["user"]=$user;
+			$_SESSION["pw"]=$pw;
+			$_SESSION["rang"]=$rang;
+			$_SESSION["nev"]=$nev;
+			if(isset( $_SESSION["rang"] ) && $_SESSION["rang"] == 1){
+				header('location: fooldal.php');
+				exit();
+			}
+			else if(isset( $_SESSION["rang"] ) && $_SESSION["rang"] == 2){
+				header('location: rendeleskezelo.php');
+				exit();
+			}
+			else if(isset( $_SESSION["rang"] ) && $_SESSION["rang"] == 3){
+				header('location: admin.php');
+				exit();
+			}
+		}
+		else{echo '<script type="text/javascript">'
+   , 'alert("Hibás felhasználónév vagy jelszó!");'
+   , '</script>';
+		}}else{
+ 		if(!isset($_SESSION["user"]) || !isset($_SESSION["pw"])){
+			echo '<form id=login name="login" method="post" action="'.$_SERVER['PHP_SELF'].'">
+ <label>BEJELENTKEZÉS</label><br /><br /><br />
+ <input type="text" name="user" title="Felhasználónév" placeholder="Felhasználónév"/><br /><br />
+ <input type="password" name="pw" title="Jelszó" placeholder="Jelszó"/><br /><br />
+<input  value="Belépés" name="belepes" id="belepes" class="button green" title="Belépés" type="submit">
+<a href="regisztracio.php" id="regisztracio" class="button red">Regisztráció</a>
+<a href="jelszoemlek.php" id="jelszoemlek">Elfelejtetted a jelszavad?</a>
+			</form>';
+			exit();
+		}
+		else{
+			echo '<form id=adat name="adat" method="post" action="'.$_SERVER['PHP_SELF'].'">
+ <label>ADATAIM</label><br /><br /><br />
+ <label>Üdvözöljük</label><br /><br />
+ <label>' . utf8_encode($_SESSION["nev"]) . '</label><br /><br />
+<a href="adatmodositas.php" id="adatmodositas" class="button red">Adatmódosítás</a>
+<a href="ujcim.php" id="ujcim" class="button red">Új cím</a>
+<a href="logout_session.php" id="kilepes" class="button red2">Kilépés</a>
+</form>';
+			exit();
+			
+
+
+
+
+		}
+	}
+require_once('footer.php');
+?>  
+</body>
+</html>
---
+++ szakdoga/regisztracio.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="hu" />
+<meta name="description" content="szakdolgozat">
+<title>Szakdolgozat</title>
+<script language=JavaScript src="ellenor.js"></script>
+</head>
+
+<body>
+<?php
+session_start();
+require_once('head.php');
+?><div id="reg">
+<h2>Regisztráció</h2>
+<form id="reg" name="reg" method="post"  onSubmit="javascript:ellenoriz()" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+<ul>	
+    <li><label>Teljes név: </label><input type="text" size="50" name="nev"  value="<?php if (isset($_POST['nev'])) {echo $_POST['nev'];} ?>" /></li>
+    <li><label>Felhasználónév: </label><input type="text" size="50" name="user" value="<?php if (isset($_POST['user'])) {echo $_POST['user'];} ?>" /></li>
+    <li><label>Jelszó: </label><input type="text" size="50" name="pw1" value="<?php if (isset($_POST['pw1'])) {echo $_POST['pw1'];} ?>"/></li>
+    <li><label>Jelszó újra: </label><input type="text" size="50" name="pw2" value="<?php if (isset($_POST['pw2'])) {echo $_POST['pw2'];} ?>"/></li>
+    <li><label>Telefonszám: </label><input type="text" size="50" name="telszam" value="<?php if (isset($_POST['telszam'])) {echo $_POST['telszam'];} ?>" />
+    </li>
+    <li><label>Email: </label><input type="text" name="email" value="<?php if (isset($_POST['email'])) {echo $_POST['email'];} ?>" /></li>
+     <input type="hidden" name="rang" value="1" />
+    <input type="submit" name="kuldes" id="reg_button" value="REGISZTRÁCIÓ" />
+    <input type="reset" name="torles" id="torles_button" value="TÖRLÉS" />
+    <input type="button" name="vissza" id="back_button" value="VISSZA" onClick="history.back()" />
+</ul>     
+</form>
+ 
+</div>
+<?php
+
+	if (isset($_POST['kuldes'])){
+		if (!empty($_POST['nev']) && !empty($_POST['user']) && !empty($_POST['pw1']) && !empty($_POST['pw2']) && !empty($_POST['telszam']) && !empty($_POST['email']) &&!empty($_POST['rang']))
+		{
+	$nev = $_POST['nev'];
+	$user = $_POST['user'];
+	$pw1 = $_POST['pw1'];
+	$pw2 = $_POST['pw2'];
+	$telszam = $_POST['telszam'];
+	$email = $_POST['email'];
+	$rang = $_POST['rang'];
+	
+	$dbc = mysqli_connect(host,user,pw,db) or die('Bukó!');
+	mysqli_query($dbc,"SET NAMES utf8");
+	$query = "select * from userek where user LIKE '%" . $user . "%'";
+	
+		$lekerdezes = mysqli_query($dbc,$query);
+		while ($adatok = mysqli_fetch_array($lekerdezes)){
+		$userellenor = $adatok['user'];}
+		if($userellenor=''){$userellenor ='a';}
+	if(strtoupper($userellenor)!=strtoupper($user)){
+	$dbc = mysqli_connect(host,user,pw,db) or die('Bukó!');
+	$query = "INSERT INTO userek (nev,user,pw,telszam,email,rang) VALUES ('$nev','$user','$pw1','$telszam','$email','$rang')";
+	mysqli_query($dbc,"SET NAMES utf8");
+	mysqli_query($dbc,$query);
+	
+	$_SESSION["user"]=$user;
+	$_SESSION["pw"]=$pw1;
+		
+	$url = 'fooldal.php?nev='.$nev.'';
+	 echo '<META http-equiv=Refresh CONTENT="0; URL='.$url.'">';
+	}
+	else{
+		echo '<script type="text/javascript">'
+   , 'alert("Válasszon másik felhasználó nevet!");'
+   , '</script>';
+	
+	}}
+	}
+
+require_once('footer.php');
+?>
+</body>
+</html>
