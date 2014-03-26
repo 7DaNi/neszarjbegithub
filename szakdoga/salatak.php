a0a099789b4288bdce61118fe296ae1d6714023b
180fok <szakdoga234@yopmail.com>
2014-03-26 21:23:52 +0100

    első salatak verzió

---
+++ szakdoga/salatak.php
@@ -0,0 +1,25 @@
+<?php
+require ("head.php");
+$con = mysqli_connect ( host, user, pw, db );
+if (mysqli_connect_errno ())
+	echo mysqli_connect_error ();
+
+$table = mysqli_query ( $con, "CREATE TABLE IF NOT EXISTS salatak(Nev CHAR(30), Ar INT, Leiras CHAR(255))" );
+mysqli_query ( $con, "SET NAMES utf8" );
+$query = mysqli_query ( $con, "SELECT * FROM salatak" );
+
+echo "\n<table id = 'salatak'>\n";
+$i = 1;
+while ( $result = mysqli_fetch_assoc ( $query ) ) {
+	if ($i == 1) {
+		echo "<tr>\n";
+	}
+	echo "<td>" . $result ['Nev'] . "</td>\n";
+	if ($i == 3) {
+		echo "</tr>\n";
+		$i = 1;
+	} else
+		$i ++;
+}
+echo "</table>\n";
+?>
